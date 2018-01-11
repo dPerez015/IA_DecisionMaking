7bf345bb09861757337e1bbd8569ae57e013f909
david lopez saludes <davidlopez1972@hotmail.es>
2018-01-12 00:48:23 +0100

    AtBank i AtHome done

--- SDL_Decisions/src/AtBank.cpp
+++ SDL_Decisions/src/AtBank.cpp
@@ -2,10 +2,28 @@
 
 void AtBank::onEnter(Agent* agent, ScenePlanning* scene) {
 	std::cout << "Entro al banco\n";
+	agent->timeCounter = 0;
 }
 
 void AtBank::Update(Agent* agent, ScenePlanning* scene) {
 	std::cout << " Estoy en el banco \n";
+	agent->timeCounter += scene->deltaTime;
+
+	if (agent->gold == 0) {
+		if (agent->wealthy < agent->maxWealthy)
+			agent->changeClass<Mining>();
+		else {
+			agent->wealthy = 0;
+			agent->changeClass<AtHome>();
+		}
+	}
+	else{
+		if (agent->timeCounter >= 1000) {
+			agent->gold--;
+			agent->wealthy += agent->gold;
+			agent->timeCounter = 0;
+		}
+	}
 }
 
 void AtBank::onExit(Agent* agent, ScenePlanning* scene) {
--- SDL_Decisions/src/AtHome.cpp
+++ SDL_Decisions/src/AtHome.cpp
@@ -2,10 +2,21 @@
 
 void AtHome::onEnter(Agent* agent, ScenePlanning* scene) {
 	std::cout << "Entro a casa\n";
+	agent->timeCounter = 0;
 }
 
 void AtHome::Update(Agent* agent, ScenePlanning* scene) {
 	std::cout << "Estoy en casa \n";
+	agent->timeCounter = scene->deltaTime;
+
+	if (agent->rested == agent->maxRested)
+		agent->changeClass<Mining>();
+	else {
+		if (agent->timeCounter >= 1000) {
+			agent->rested++;
+			agent->timeCounter = 0;
+		}
+	}
 	
 }
 
