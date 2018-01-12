6ee14f799d773f1ea315dcc727c348b887cbca36
david lopez saludes <davidlopez1972@hotmail.es>
2018-01-12 17:28:47 +0100

    couts extrets

--- SDL_Decisions/src/AtHome.cpp
+++ SDL_Decisions/src/AtHome.cpp
@@ -1,7 +1,6 @@
 #include "AtHome.h"
 
 void AtHome::onEnter(Agent* agent, ScenePlanning* scene) {
-	std::cout << "Entro a casa\n";
 	agent->timeCounter = 0;
 	agent->wealthy = 0;
 	Vector2D posHome = {550, 520};
@@ -9,7 +8,6 @@ void AtHome::onEnter(Agent* agent, ScenePlanning* scene) {
 }
 
 void AtHome::Update(Agent* agent, ScenePlanning* scene) {
-	std::cout << "Estoy en casa \n";
 	agent->timeCounter += scene->deltaTime;
 
 	if (agent->rested == agent->maxRested)
@@ -24,5 +22,4 @@ void AtHome::Update(Agent* agent, ScenePlanning* scene) {
 }
 
 void AtHome::onExit(Agent* agent, ScenePlanning* scene) {
-	std::cout << "Salgo de casa\n";
 }
--- SDL_Decisions/src/AtSaloon.cpp
+++ SDL_Decisions/src/AtSaloon.cpp
@@ -2,14 +2,12 @@
 
 
 void AtSaloon::onEnter(Agent* agent, ScenePlanning* scene) {
-	std::cout << "Entro al bar\n";
 	agent->timeCounter = 0;
 	Vector2D posSaloon = {100, 520};
 	scene->path.points = Aestrella::search(scene->findInGraph(agent->getPosition()), scene->pix2cell(posSaloon));
 }
 
 void AtSaloon::Update(Agent* agent, ScenePlanning* scene) {
-	std::cout << "estoy en el bar\n";
 	agent->timeCounter += scene->deltaTime;
 
 	if (agent->thirsty >= 10) { //10 es max thirsty
@@ -24,5 +22,4 @@ void AtSaloon::Update(Agent* agent, ScenePlanning* scene) {
 }
 
 void AtSaloon::onExit(Agent* agent, ScenePlanning* scene) {
-	std::cout << "me voy del bar";
 }
